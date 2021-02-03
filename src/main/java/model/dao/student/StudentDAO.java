361642761956dfa230e41c47b04f9ce18ea39c85
Dimpeny <guilhermezimermann.grz@gmail.com>
2021-02-03 14:02:15 -0300

    Created StudentDAO and StudentDAOImpl

---
+++ src/main/java/model/dao/student/StudentDAO.java
@@ -0,0 +1,17 @@
+package model.dao.student;
+
+import java.util.List;
+
+import model.entity.student.Student;
+
+public interface StudentDAO {
+
+void insertStudent(Student student);
+	
+	void removeStudent(Student student);
+		
+	void updateStudent(Student student);
+		
+	List<Student> listStudent();
+	
+}
---
+++ src/main/java/model/dao/student/StudentDAOImpl.java
@@ -0,0 +1,150 @@
+package model.dao.student;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+import org.hibernate.Session;
+
+import java.util.List;
+import model.entity.student.Student;
+import model.factory.connection.ConnectionFactory;
+
+
+public class StudentDAOImpl implements StudentDAO {
+	
+	private ConnectionFactory factory;
+
+	public StudentDAOImpl() {
+		factory = new ConnectionFactory();
+	}
+	
+	public void insertStudent(Student student) {
+
+		Session session = null;
+
+		try {
+
+			session = factory().openSession();
+			session.beginTransaction();
+
+			session.save(student);
+			
+			session.getTransaction().commit();
+
+		} catch (Exception sqlException) {
+
+			sqlException.printStackTrace();
+
+			if (session.getTransaction() != null) {
+				session.getTransaction().rollback();
+			}
+
+		} finally {
+
+			if (session != null) {
+				session.close();
+			}
+		}
+
+	}
+
+	public void removeStudent(Student student) {
+
+		Session session = null;
+
+		try {
+
+			session = factory().openSession();
+			session.beginTransaction();
+
+			session.remove(student);
+
+			session.getTransaction().commit();
+
+		} catch (Exception sqlException) {
+
+			sqlException.printStackTrace();
+
+			if (session.getTransaction() != null) {
+				session.getTransaction().rollback();
+			}
+
+		} finally {
+
+			if (session != null) {
+				session.close();
+			}
+		}
+
+	}
+
+	public void updateStudent(Student student) {
+
+		Session session = null;
+
+		try {
+
+			session = factory().openSession();
+			session.beginTransaction();
+
+			session.update(student);
+
+			session.getTransaction().commit();
+
+		} catch (Exception sqlException) {
+
+			sqlException.printStackTrace();
+
+			if (session.getTransaction() != null) {
+				session.getTransaction().rollback();
+			}
+
+		} finally {
+
+			if (session != null) {
+				session.close();
+			}
+		}
+	}
+
+	public List<Student> listStudent() {
+
+		Session session = null;
+		List<Student> student = null;
+
+		try {
+
+			session = factory().openSession();
+			session.beginTransaction();
+
+			CriteriaBuilder construtor = session.getCriteriaBuilder();
+
+			CriteriaQuery<Student> criteria = construtor.createQuery(Student.class);
+			Root<Student> raizCliente = criteria.from(Student.class);
+
+			criteria.select(raizCliente);
+
+			student = session.createQuery(criteria).getResultList();
+
+			session.getTransaction().commit();
+
+		} catch (Exception sqlException) {
+
+			sqlException.printStackTrace();
+
+			if (session.getTransaction() != null) {
+				session.getTransaction().rollback();
+			}
+
+		} finally {
+
+			if (session != null) {
+				session.close();
+			}
+		}
+
+		return student;
+
+	}
+}
