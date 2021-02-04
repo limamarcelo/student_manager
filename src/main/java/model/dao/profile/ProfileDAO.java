e6dc116e9bc2f5a4f470c60bc98e880695eef1a0
1gui <guilherme76.gm@gmail.com>
2021-02-04 16:08:52 -0300

    DAO Class Profile

---
+++ src/main/java/model/dao/profile/ProfileDAO.java
@@ -0,0 +1,11 @@
+package model.dao.profile;
+
+public interface ProfileDAO {
+	
+	void insertProfile(Profile profile);
+	
+	void deleteProfile(Profile profile);
+	
+	void updateProfile(Profile profile);
+
+}
---
+++ src/main/java/model/dao/profile/ProfileDAOImpl.java
@@ -0,0 +1,99 @@
+package model.dao.profile;
+
+public class ProfileDAOImpl implements ProfileDAO {
+	
+	private ConnectionFactory factory;
+
+	public ProfileDAOImpl() {
+		factory = new ConnectionFactory();
+
+	public void insertProfile(Profile profile) {
+		
+		Session session = null;
+
+		try {
+
+			session = factory().openSession();
+			session.beginTransaction();
+
+			session.save(profile);
+			
+			session.getTransaction().commit();
+
+		} catch (Exception sqlException) {
+
+			sqlException.printStackTrace();
+
+			if (session.getTransaction() != null) {
+				session.getTransaction().rollback();
+			}
+
+		} finally {
+
+			if (session != null) {
+				session.close();
+			}
+		}
+
+	}
+
+	public void deleteProfile(Profile profile) {
+		
+		Session session = null;
+
+		try {
+
+			session = factory().openSession();
+			session.beginTransaction();
+
+			session.remove(profile);
+
+			session.getTransaction().commit();
+
+		} catch (Exception sqlException) {
+
+			sqlException.printStackTrace();
+
+			if (session.getTransaction() != null) {
+				session.getTransaction().rollback();
+			}
+
+		} finally {
+
+			if (session != null) {
+				session.close();
+			}
+		}
+
+	}
+
+	public void updateProfile(Profile profile) {
+		
+		Session session = null;
+
+		try {
+
+			session = factory().openSession();
+			session.beginTransaction();
+
+			session.update(profile);
+
+			session.getTransaction().commit();
+
+		} catch (Exception sqlException) {
+
+			sqlException.printStackTrace();
+
+			if (session.getTransaction() != null) {
+				session.getTransaction().rollback();
+			}
+
+		} finally {
+
+			if (session != null) {
+				session.close();
+			}
+		}
+	}
+}
+}
