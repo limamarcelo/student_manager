7952448637b15ccd884bcda2c52af62791dbf679
Dimpeny <guilhermezimermann.grz@gmail.com>
2021-02-05 13:58:47 -0300

    Changed 'long' to 'Long' in Teacher.java

--- src/main/java/model/entity/teacher/Teacher.java
+++ src/main/java/model/entity/teacher/Teacher.java
@@ -14,7 +14,7 @@ public class Teacher {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id_teacher")
-	private  long id;
+	private  Long id;
 	
 	@Column(name = "name_teacher", length = 25, nullable = false, unique = false)
 	private String name;
