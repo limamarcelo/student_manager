996692013737518dc9acd640e9ab61e058edd6cf
Horizonete <ardilosogamer@gmail.com>
2021-02-05 17:24:06 -0300

    frequency class dao tidy

---
+++ src/main/java/model/dao/frequency/FrequencyDAOImpl.java
@@ -0,0 +1,151 @@
+package model.dao.frequency;
+
+import java.util.List;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+import org.hibernate.Session;
+
+import model.entity.frequency.Frequency;
+import model.factory.connection.ConnectionFactory;S
+
+public class FrequencyDAOImpl implements FrequencyDAO {
+
+	private ConnectionFactory factory;
+
+	public FrequencyDAOImpl() {
+		factory = new ConnectionFactory();
+		
+	}
+	
+	public void insertFrequency(Frequency frequency) {
+		
+		Session session = null;
+
+		try {
+
+			session = factory().openSession();
+			session.beginTransaction();
+
+			session.save(frequency);
+			
+			session.getTransaction().commit();
+
+		} catch (Exception sqlException) {
+
+			sqlException.printStackTrace();
+
+			if (session.getTransaction() != null) {
+				session.getTransaction().rollback();
+			}
+
+		} finally {
+
+			if (session != null) {
+				session.close();
+			}
+		}
+
+	}
+
+	public void removeFrequency(Frequency frequency) {
+		
+		Session session = null;
+
+		try {
+
+			session = factory().openSession();
+			session.beginTransaction();
+
+			session.remove(frequency);
+
+			session.getTransaction().commit();
+
+		} catch (Exception sqlException) {
+
+			sqlException.printStackTrace();
+
+			if (session.getTransaction() != null) {
+				session.getTransaction().rollback();
+			}
+
+		} finally {
+
+			if (session != null) {
+				session.close();
+			}
+		}
+
+	}
+
+	public void updateFrequency(Frequency frequency) {
+		
+		Session session = null;
+
+		try {
+
+			session = factory().openSession();
+			session.beginTransaction();
+
+			session.update(frequency);
+
+			session.getTransaction().commit();
+
+		} catch (Exception sqlException) {
+
+			sqlException.printStackTrace();
+
+			if (session.getTransaction() != null) {
+				session.getTransaction().rollback();
+			}
+
+		} finally {
+
+			if (session != null) {
+				session.close();
+			}
+		}
+	}
+	
+	public List<Frequency> listFrequency() {
+
+		Session session = null;
+		List<Frequency> user = null;
+
+		try {
+
+			session = factory().openSession();
+			session.beginTransaction();
+
+			CriteriaBuilder construtor = session.getCriteriaBuilder();
+
+			CriteriaQuery<Frequency> criteria = construtor.createQuery(Frequency.class);
+			Root<Frequency> rootClient = criteria.from(Frequency.class);
+
+			criteria.select(rootClient);
+
+			user = session.createQuery(criteria).getResultList();
+
+			session.getTransaction().commit();
+
+		} catch (Exception sqlException) {
+
+			sqlException.printStackTrace();
+
+			if (session.getTransaction() != null) {
+				session.getTransaction().rollback();
+			}
+
+		} finally {
+
+			if (session != null) {
+				session.close();
+			}
+		}
+
+		return user;
+
+	}
+}
