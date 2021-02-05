bcedde8a1f6d67baa11092896092a7df30fd793d
Marcelo Miranda de Lima <miranda.rs@gmail.com>
2021-02-05 17:11:19 -0300

    Deleted blanck lines and incorrect import removed

--- src/main/java/model/entity/student/Student.java
+++ src/main/java/model/entity/student/Student.java
@@ -1,7 +1,5 @@
 package model.entity.student;
 
-import model.entity.company.Company;
-import model.entity.course.Course;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -12,6 +10,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import model.entity.company.Company;
 import model.entity.course.Course;
 
 @Entity
@@ -114,6 +113,4 @@ public class Student {
 	public void setCompany(Company company) {
 		this.company = company;
 	}
-	
-
 }
